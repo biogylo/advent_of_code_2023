b39c805618b9a5323fe2473ed5b6a4b12fe1a7e1
Juan Francisco Navarro Rodriguez <navarro.juan@uabc.edu.mx>
2023-12-02 23:53:18 -0800

    Pass the advent of code day 2!

--- src/cube_conundrum.rs
+++ src/cube_conundrum.rs
@@ -1,18 +1,33 @@
+use std::collections::HashMap;
 use std::str::FromStr;
-
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
+#[derive(Debug, Eq, PartialEq, Hash, EnumIter, Clone)]
 pub enum CubeColor {
     Red,
     Blue,
     Green,
 }
+#[derive(Eq, Debug, PartialEq)]
+pub struct CubeCounts(HashMap<CubeColor, usize>);
 
-#[derive(PartialEq, Debug)]
-pub struct Turn {
-    pub red_balls_shown: usize,
-    pub green_balls_shown: usize,
-    pub blue_balls_shown: usize,
+impl FromIterator<(CubeColor, usize)> for CubeCounts {
+    fn from_iter<T: IntoIterator<Item = (CubeColor, usize)>>(iter: T) -> Self {
+        let mut the_hashmap = HashMap::from_iter(iter);
+        for variant in CubeColor::iter() {
+            if !the_hashmap.contains_key(&variant) {
+                the_hashmap.insert(variant, 0);
+            }
+        }
+        return CubeCounts { 0: the_hashmap };
+    }
 }
 
+impl<const N: usize> From<[(CubeColor, usize); N]> for CubeCounts {
+    fn from(cubecolor_usize_array: [(CubeColor, usize); N]) -> Self {
+        cubecolor_usize_array.into_iter().collect()
+    }
+}
 impl FromStr for CubeColor {
     type Err = ();
 
@@ -32,37 +47,128 @@ fn get_elements_from_turn_token(token: &str) -> Option<(CubeColor, usize)> {
     let count = usize::from_str(number_token).ok()?;
     Some((color, count))
 }
-impl FromStr for Turn {
-    type Err = ();
 
+impl CubeCounts {
+    pub fn get(&self, color: &CubeColor) -> usize {
+        self.0[&color]
+    }
+    /*
+        Whether the given cube count fits the other one
+    */
+    pub fn fits(&self, bag_other: &Self) -> bool {
+        for color in CubeColor::iter() {
+            if self.0[&color] > bag_other.0[&color] {
+                return false;
+            }
+        }
+        return true;
+    }
     /*
         Takes in a line like " 3 blue, 4 red",
         and returns a Turn with the correct fields
     */
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut the_turn = Self {
-            red_balls_shown: 0,
-            green_balls_shown: 0,
-            blue_balls_shown: 0,
-        };
-        let elements = s.trim().split(',').map(get_elements_from_turn_token);
-        for element in elements {
-            match element {
-                Some((CubeColor::Red, count)) => the_turn.red_balls_shown = count,
-                Some((CubeColor::Green, count)) => the_turn.green_balls_shown = count,
-                Some((CubeColor::Blue, count)) => the_turn.blue_balls_shown = count,
-                None => return Err(()),
-            }
-        }
-        return Ok(the_turn);
+    pub fn from_turn_str(turn_string: &str) -> Result<CubeCounts, String> {
+        let turn_vector: Vec<(CubeColor, usize)> = turn_string
+            .trim()
+            .split(',')
+            .map(get_elements_from_turn_token)
+            .collect::<Option<Vec<(CubeColor, usize)>>>()
+            .ok_or(String::from("Unable to parse turn string"))?;
+        return Ok(CubeCounts::from_iter(turn_vector.into_iter()));
+    }
+
+    pub fn power(&self) -> usize {
+        CubeColor::iter().map(|color| self.get(&color)).product()
     }
 }
 
-pub struct GameRecord {
-    all_turns: Vec<Turn>,
+#[derive(Eq, PartialEq, Debug)]
+pub struct Game {
+    pub id: usize,
+    pub turns: Vec<CubeCounts>,
 }
-pub struct Bag {
-    red_cubes: usize,
-    blue_cubes: usize,
-    green_cubes: usize,
+
+#[derive(Eq, PartialEq, Debug)]
+pub struct Conundrum(pub Vec<Game>);
+impl FromStr for Conundrum {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Self {
+            0: s.trim()
+                .split("\n")
+                .map(Game::from_str)
+                .collect::<Result<Vec<Game>, _>>()?,
+        })
+    }
+}
+impl Conundrum {
+    pub fn possible_games(&self, bag: &CubeCounts) -> Vec<usize> {
+        self.0
+            .iter()
+            .filter(|game| game.is_possible(bag))
+            .map(|game| game.id)
+            .collect()
+    }
+
+    pub fn sum_of_possible_game_ids(&self, bag: &CubeCounts) -> usize {
+        self.possible_games(bag).iter().sum()
+    }
+
+    pub fn smallest_bags_needed(&self) -> Vec<CubeCounts> {
+        self.0.iter().map(Game::smallest_bag).collect()
+    }
+
+    pub fn powers(&self) -> Vec<usize> {
+        self.smallest_bags_needed()
+            .iter()
+            .map(CubeCounts::power)
+            .collect()
+    }
+}
+impl Game {
+    /*
+        Tells you whether the game can be done with the
+        given bag of cube counts.
+    */
+    pub fn is_possible(&self, bag: &CubeCounts) -> bool {
+        self.turns.iter().all(|turn| turn.fits(&bag))
+    }
+
+    pub fn smallest_bag(&self) -> CubeCounts {
+        CubeColor::iter()
+            .map(|color| {
+                (
+                    color.clone(),
+                    self.turns
+                        .iter()
+                        .map(|turn| turn.get(&color))
+                        .max()
+                        .unwrap(),
+                )
+            })
+            .collect()
+    }
+}
+
+impl FromStr for Game {
+    type Err = String;
+    fn from_str(game_string: &str) -> Result<Self, String> {
+        let right_string = game_string.trim().replace("Game", "");
+        let (game_id_token, turn_tokens) = right_string
+            .trim()
+            .split_once(':')
+            .ok_or("Unable to parse game str due to missing colon".to_string())?;
+        let game_id: usize = game_id_token
+            .parse()
+            .or(Err("Unable to parse game id".to_string()))?;
+        let turn_cube_counts: Result<Vec<CubeCounts>, String> = turn_tokens
+            .split(";")
+            .map(|turn_token| CubeCounts::from_turn_str(turn_token))
+            .collect();
+        Ok(Self {
+            id: game_id,
+            turns: turn_cube_counts?,
+        })
+    }
 }
--- tests/test_day_2_cube_conundrum.rs
+++ tests/test_day_2_cube_conundrum.rs
@@ -1,30 +1,264 @@
 #[cfg(test)]
 mod tests {
-    use advent_of_code_2023::cube_conundrum::{Bag, CubeColor, Turn};
-    use std::collections::HashMap;
+    use advent_of_code_2023::cube_conundrum::{Conundrum, CubeColor, CubeCounts, Game};
+    use std::fs;
     use std::str::FromStr;
     #[test]
     fn parse_turn() {
-        let parsed_turn = Turn::from_str(" 3 blue, 4 red").unwrap();
+        let parsed_turn = CubeCounts::from_turn_str(" 3 blue, 4 red").unwrap();
 
-        let desired_turn = Turn {
-            red_balls_shown: 4,
-            green_balls_shown: 0,
-            blue_balls_shown: 3,
-        };
+        let desired_turn = CubeCounts::from([
+            (CubeColor::Red, 4),
+            (CubeColor::Green, 0),
+            (CubeColor::Blue, 3),
+        ]);
 
         assert_eq!(desired_turn, parsed_turn);
     }
     #[test]
-    fn parse_turn_2() {
-        let parsed_turn = Turn::from_str(" 50 blue, 5   green,   4 red   ").unwrap();
+    fn parse_turn_ugly_case() {
+        let parsed_turn = CubeCounts::from_turn_str(" 50 blue, 5   green,   4 red   ").unwrap();
 
-        let desired_turn = Turn {
-            red_balls_shown: 4,
-            green_balls_shown: 5,
-            blue_balls_shown: 50,
-        };
+        let desired_turn = CubeCounts::from([
+            (CubeColor::Red, 4),
+            (CubeColor::Green, 5),
+            (CubeColor::Blue, 50),
+        ]);
 
         assert_eq!(desired_turn, parsed_turn);
+    }
+
+    #[test]
+    fn cube_counts_power_implemented() {
+        let counts = CubeCounts::from([
+            (CubeColor::Red, 4),
+            (CubeColor::Green, 5),
+            (CubeColor::Blue, 50),
+        ]);
+
+        assert_eq!(counts.power(), 4 * 5 * 50);
+    }
+    #[test]
+    fn parse_game_1() {
+        let parsed_game: Game = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+            .parse()
+            .unwrap();
+        let desired_game = Game {
+            id: 1,
+            turns: vec![
+                CubeCounts::from([(CubeColor::Red, 4), (CubeColor::Blue, 3)]),
+                CubeCounts::from([
+                    (CubeColor::Red, 1),
+                    (CubeColor::Green, 2),
+                    (CubeColor::Blue, 6),
+                ]),
+                CubeCounts::from([(CubeColor::Green, 2)]),
+            ],
+        };
+
+        assert_eq!(desired_game, parsed_game);
+    }
+
+    #[test]
+    fn parse_game_3() {
+        let parsed_game = Game::from_str(
+            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+        )
+        .unwrap();
+        let desired_game = Game {
+            id: 3,
+            turns: vec![
+                CubeCounts::from([
+                    (CubeColor::Green, 8),
+                    (CubeColor::Red, 20),
+                    (CubeColor::Blue, 6),
+                ]),
+                CubeCounts::from([
+                    (CubeColor::Red, 4),
+                    (CubeColor::Green, 13),
+                    (CubeColor::Blue, 5),
+                ]),
+                CubeCounts::from([(CubeColor::Red, 1), (CubeColor::Green, 5)]),
+            ],
+        };
+
+        assert_eq!(desired_game, parsed_game);
+    }
+
+    #[test]
+    fn bag_with_less_greens_cant_do_game_with_more() {
+        let the_game = Game {
+            id: 3,
+            turns: vec![
+                CubeCounts::from([
+                    (CubeColor::Green, 8),
+                    (CubeColor::Red, 20),
+                    (CubeColor::Blue, 6),
+                ]),
+                CubeCounts::from([
+                    (CubeColor::Red, 4),
+                    (CubeColor::Green, 13),
+                    (CubeColor::Blue, 5),
+                ]),
+                CubeCounts::from([(CubeColor::Red, 1), (CubeColor::Green, 5)]),
+            ],
+        };
+        let bag: CubeCounts = CubeCounts::from([
+            (CubeColor::Green, 2),
+            (CubeColor::Red, 999),
+            (CubeColor::Blue, 999),
+        ]);
+        assert!(!the_game.is_possible(&bag));
+    }
+
+    #[test]
+    fn big_bag_can_do_game() {
+        let the_game = Game {
+            id: 3,
+            turns: vec![
+                CubeCounts::from([
+                    (CubeColor::Green, 8),
+                    (CubeColor::Red, 20),
+                    (CubeColor::Blue, 6),
+                ]),
+                CubeCounts::from([
+                    (CubeColor::Red, 4),
+                    (CubeColor::Green, 13),
+                    (CubeColor::Blue, 5),
+                ]),
+                CubeCounts::from([(CubeColor::Red, 1), (CubeColor::Green, 5)]),
+            ],
+        };
+        let bag: CubeCounts = CubeCounts::from([
+            (CubeColor::Green, 999),
+            (CubeColor::Red, 999),
+            (CubeColor::Blue, 999),
+        ]);
+        assert!(the_game.is_possible(&bag));
+    }
+
+    #[test]
+    fn fewest_possible_cubes_to_do_game() {
+        let the_game = Game {
+            id: 12,
+            turns: vec![
+                CubeCounts::from([
+                    (CubeColor::Red, 20),
+                    (CubeColor::Green, 8),
+                    (CubeColor::Blue, 6),
+                ]),
+                CubeCounts::from([
+                    (CubeColor::Red, 4),
+                    (CubeColor::Green, 13),
+                    (CubeColor::Blue, 5),
+                ]),
+                CubeCounts::from([
+                    (CubeColor::Red, 10),
+                    (CubeColor::Green, 10),
+                    (CubeColor::Blue, 30),
+                ]),
+            ],
+        };
+        let expected_bag: CubeCounts = CubeCounts::from([
+            (CubeColor::Green, 13),
+            (CubeColor::Red, 20),
+            (CubeColor::Blue, 30),
+        ]);
+        assert_eq!(the_game.smallest_bag(), expected_bag);
+    }
+
+    #[test]
+    fn two_game_conundrum_gets_parsed_correctly() {
+        let parsed_conundrum:Conundrum = "\n Game 4: 1 green,  3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green\n".parse().unwrap();
+
+        let expected_conundrum = Conundrum {
+            0: vec![
+                Game {
+                    id: 4,
+                    turns: vec![
+                        CubeCounts::from([
+                            (CubeColor::Red, 3),
+                            (CubeColor::Green, 1),
+                            (CubeColor::Blue, 6),
+                        ]),
+                        CubeCounts::from([(CubeColor::Red, 6), (CubeColor::Green, 3)]),
+                        CubeCounts::from([
+                            (CubeColor::Red, 14),
+                            (CubeColor::Green, 3),
+                            (CubeColor::Blue, 15),
+                        ]),
+                    ],
+                },
+                Game {
+                    id: 5,
+                    turns: vec![
+                        CubeCounts::from([
+                            (CubeColor::Red, 6),
+                            (CubeColor::Green, 3),
+                            (CubeColor::Blue, 1),
+                        ]),
+                        CubeCounts::from([
+                            (CubeColor::Red, 1),
+                            (CubeColor::Green, 2),
+                            (CubeColor::Blue, 2),
+                        ]),
+                    ],
+                },
+            ],
+        };
+        assert_eq!(expected_conundrum, parsed_conundrum);
+    }
+
+    #[test]
+    fn possible_games_from_challenge_example_are_correct() {
+        let conundrum: Conundrum = fs::read_to_string("./data/cube_conundrum_input_short.txt")
+            .unwrap()
+            .parse()
+            .unwrap();
+        let bag: CubeCounts = CubeCounts::from([
+            (CubeColor::Red, 12),
+            (CubeColor::Green, 13),
+            (CubeColor::Blue, 14),
+        ]);
+        assert_eq!(conundrum.possible_games(&bag), vec![1, 2, 5]);
+        assert_eq!(conundrum.sum_of_possible_game_ids(&bag), 8);
+    }
+
+    #[test]
+    fn possible_games_from_challenge_part1_are_correct() {
+        let conundrum: Conundrum = fs::read_to_string("./data/cube_conundrum_input_mid.txt")
+            .unwrap()
+            .parse()
+            .unwrap();
+        let bag: CubeCounts = CubeCounts::from([
+            (CubeColor::Red, 12),
+            (CubeColor::Green, 13),
+            (CubeColor::Blue, 14),
+        ]);
+        assert_eq!(conundrum.sum_of_possible_game_ids(&bag), 2528);
+    }
+
+    #[test]
+    fn powers_of_each_games_minimum_bag_added_up_are_correct_for_the_short_example() {
+        let conundrum: Conundrum = fs::read_to_string("./data/cube_conundrum_input_short.txt")
+            .unwrap()
+            .parse()
+            .unwrap();
+
+        let powers: Vec<usize> = conundrum.powers();
+        assert_eq!(powers, vec![48, 12, 1560, 630, 36]);
+        let sum: usize = powers.iter().sum();
+        assert_eq!(sum, 2286);
+    }
+
+    #[test]
+    fn powers_of_each_games_minimum_bag_added_up_are_correct_for_the_mid_example() {
+        let conundrum: Conundrum = fs::read_to_string("./data/cube_conundrum_input_mid.txt")
+            .unwrap()
+            .parse()
+            .unwrap();
+
+        let sum: usize = conundrum.powers().iter().sum();
+        assert_eq!(sum, 67363);
     }
 }
