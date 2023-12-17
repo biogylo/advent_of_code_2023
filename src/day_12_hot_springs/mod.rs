2fc16357069ae8d9c27e34b4dd46e267cc42a1ec
Juan Francisco Navarro Rodriguez <navarro.juan@uabc.edu.mx>
2023-12-16 22:32:58 -0800

    Works without memoization

--- src/day_12_hot_springs/mod.rs
+++ src/day_12_hot_springs/mod.rs
@@ -22,12 +22,6 @@ pub struct HotSpringsMemo {
     memory: HashMap<StringAndVectorKey, usize>,
 }
 
-enum MatchResult<'a> {
-    Matched,
-    NotMatched,
-    Rest(&'a [char]),
-}
-
 impl HotSpringsMemo {
     const UNKNOWN: char = '?';
     const OPERATIONAL: char = '.';
@@ -38,7 +32,11 @@ impl HotSpringsMemo {
         }
     }
 
-    pub fn get_arrangements_for_row(&mut self, the_row: &str) -> Result<usize, String> {
+    pub fn get_arrangements_for_row(
+        &mut self,
+        the_row: &str,
+        unfold_n: usize,
+    ) -> Result<usize, String> {
         let (condition_record, count_tokens) = the_row
             .trim()
             .split_once(" ")
@@ -59,22 +57,49 @@ impl HotSpringsMemo {
             .map(|token| token.parse())
             .collect::<Result<Vec<usize>, _>>()
             .map_err(|_| "Unable to parse all number tokens from a row")?;
-        let condition_record_char_array = condition_record.chars().collect_vec();
+        let record_array = condition_record.chars().collect_vec();
 
-        return Ok(
-            self.count_matching_patterns(condition_record_char_array.as_slice(), counts.as_slice())
-        );
+        let mut unfolded_record_array = record_array.clone();
+        let mut unfolded_counts = counts.clone();
+
+        for i in (1..unfold_n) {
+            unfolded_record_array.push(HotSpringsMemo::UNKNOWN);
+            unfolded_record_array.extend(record_array.clone());
+            unfolded_counts.extend(counts.clone());
+        }
+
+        return Ok(HotSpringsMemo::count_matching_patterns(
+            unfolded_record_array.as_slice(),
+            unfolded_counts.as_slice(),
+        ));
     }
 
-    pub fn get_arrangements_for_paragraph(&mut self, the_paragraph: &str) -> Result<usize, String> {
+    pub fn get_arrangements_for_paragraph(
+        &mut self,
+        the_paragraph: &str,
+        unfold_n: usize,
+    ) -> Result<usize, String> {
         the_paragraph
             .trim()
             .lines()
-            .map(|the_row| self.get_arrangements_for_row(the_row))
+            .map(|the_row| self.get_arrangements_for_row(the_row, unfold_n))
             .fold_ok(0, Add::add)
     }
-    fn count_matching_patterns(&mut self, the_string: &[char], the_vector: &[usize]) -> usize {
-        let mut matches = 0;
+
+    fn cached_count_matching_patters(
+        &mut self,
+        the_string: &[char],
+        the_vector: &[usize],
+    ) -> usize {
+        let key = StringAndVectorKey::from_parameters(the_string, the_vector);
+        if let Some(cache) = self.memory.get(&key) {
+            return *cache;
+        }
+        let result = HotSpringsMemo::count_matching_patterns(the_string, the_vector);
+        self.memory.insert(key, result);
+        result
+    }
+    fn count_matching_patterns(the_string: &[char], the_vector: &[usize]) -> usize {
         if the_vector.len() == 0 {
             if the_string.len() == 0 {
                 // Defo a match
@@ -85,8 +110,10 @@ impl HotSpringsMemo {
                 return 0;
             }
         }
+        let mut matches = 0;
         let match_count = the_vector[0];
         let rest_vec = &the_vector[1..];
+
         for (i, _) in the_string.windows(match_count).enumerate() {
             if the_string[i..i + match_count]
                 .iter()
@@ -96,8 +123,10 @@ impl HotSpringsMemo {
                 if let Some(next_char) = the_string.get(i + match_count) {
                     if next_char != &HotSpringsMemo::DAMAGED {
                         // This could very well be a match, but only if the rest is a match
-                        matches += self
-                            .count_matching_patterns(&the_string[i + match_count + 1..], rest_vec);
+                        matches += HotSpringsMemo::count_matching_patterns(
+                            &the_string[i + match_count + 1..],
+                            rest_vec,
+                        );
                     } else {
                         // Not a match... We need to iterate further
                     }
--- tests/test_day_12_hot_springs.rs
+++ tests/test_day_12_hot_springs.rs
@@ -1,11 +1,12 @@
 #[cfg(test)]
 mod tests {
     use advent_of_code_2023::day_12_hot_springs::HotSpringsMemo;
+    use std::fs;
 
     #[test]
     fn small_pattern_works() {
         let mut memo = HotSpringsMemo::new();
-        let arrangements = memo.get_arrangements_for_row("? 1").unwrap();
+        let arrangements = memo.get_arrangements_for_row("? 1", 1).unwrap();
         assert_eq!(arrangements, 1);
         //
     }
@@ -13,7 +14,7 @@ mod tests {
     #[test]
     fn small_pattern_2_works() {
         let mut memo = HotSpringsMemo::new();
-        let arrangements = memo.get_arrangements_for_row("?? 1").unwrap();
+        let arrangements = memo.get_arrangements_for_row("?? 1", 1).unwrap();
         assert_eq!(arrangements, 2);
         //
     }
@@ -21,7 +22,7 @@ mod tests {
     #[test]
     fn small_pattern_3_works() {
         let mut memo = HotSpringsMemo::new();
-        let arrangements = memo.get_arrangements_for_row("???? 1,2").unwrap();
+        let arrangements = memo.get_arrangements_for_row("???? 1,2", 1).unwrap();
         assert_eq!(arrangements, 1);
         //
     }
@@ -29,8 +30,62 @@ mod tests {
     #[test]
     fn small_pattern_4_works() {
         let mut memo = HotSpringsMemo::new();
-        let arrangements = memo.get_arrangements_for_row("#???#? 1,2").unwrap();
+        let arrangements = memo.get_arrangements_for_row("#???#? 1,2", 1).unwrap();
         assert_eq!(arrangements, 2);
+        //
+    }
+
+    #[test]
+    fn small_pattern_5_works() {
+        let mut memo = HotSpringsMemo::new();
+        let arrangements = memo
+            .get_arrangements_for_row("?###???????? 3,2,1", 1)
+            .unwrap();
+        assert_eq!(arrangements, 10);
+        //
+    }
+
+    #[test]
+    fn test_case_from_challenge_part_1_works() {
+        let the_paragraph = fs::read_to_string("./data/hot_springs_input_short.txt").unwrap();
+        let mut memo = HotSpringsMemo::new();
+        let arrangements = memo
+            .get_arrangements_for_paragraph(the_paragraph.as_str(), 1)
+            .unwrap();
+        assert_eq!(arrangements, 21);
+        //
+    }
+
+    #[test]
+    fn challenge_part_1_full_input_works() {
+        let the_paragraph = fs::read_to_string("./data/hot_springs_input_long.txt").unwrap();
+        let mut memo = HotSpringsMemo::new();
+        let arrangements = memo
+            .get_arrangements_for_paragraph(the_paragraph.as_str(), 1)
+            .unwrap();
+        assert_eq!(arrangements, 7771);
+        //
+    }
+
+    #[test]
+    fn test_case_from_challenge_part_2_works() {
+        let the_paragraph = fs::read_to_string("./data/hot_springs_input_short.txt").unwrap();
+        let mut memo = HotSpringsMemo::new();
+        let arrangements = memo
+            .get_arrangements_for_paragraph(the_paragraph.as_str(), 5)
+            .unwrap();
+        assert_eq!(arrangements, 525152);
+        //
+    }
+
+    #[test]
+    fn challenge_part_2_full_input_works() {
+        let the_paragraph = fs::read_to_string("./data/hot_springs_input_long.txt").unwrap();
+        let mut memo = HotSpringsMemo::new();
+        let arrangements = memo
+            .get_arrangements_for_paragraph(the_paragraph.as_str(), 5)
+            .unwrap();
+        assert_eq!(arrangements, 0);
         //
     }
 }
