955cbd6c9abc752a3463f7cc9e4cc99341c52add
Juan Francisco Navarro Rodriguez <navarro.juan@uabc.edu.mx>
2023-12-18 00:12:19 -0800

    Add another test case

--- src/sandbox/dijkstra.rs
+++ src/sandbox/dijkstra.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+use std::collections::HashSet;
 
 pub struct Graph {
     adjacency_grid: Vec<Vec<Option<usize>>>,
@@ -35,19 +36,24 @@ impl Graph {
         adjacent_nodes
     }
     pub fn shortest_distance(&self, node_a: usize, node_b: usize) -> usize {
-        let mut visited_nodes: Vec<bool> = vec![false; self.number_of_nodes];
+        let mut unvisited_nodes: HashSet<usize> = HashSet::new();
+        (0..self.number_of_nodes).for_each(|node| {
+            unvisited_nodes.insert(node);
+        });
         let mut shortest_distance: Vec<usize> = vec![usize::MAX; self.number_of_nodes];
         let mut current_node = node_a;
 
         // Start setting values for our start point
         shortest_distance[node_a] = 0; // Duh
-        visited_nodes[node_a] = true;
 
         // We will go through all adjacent non-visited nodes
         loop {
-            visited_nodes[current_node] = true;
+            unvisited_nodes.remove(&current_node);
             let adjacent_nodes = self.get_adjacent_nodes(current_node);
-            for adjacent_node in adjacent_nodes.iter().filter(|&node| !visited_nodes[*node]) {
+            for adjacent_node in adjacent_nodes
+                .iter()
+                .filter(|&node| unvisited_nodes.contains(node))
+            {
                 let distance = shortest_distance[current_node]
                     + self.adjacency_grid[current_node][*adjacent_node]
                         .expect("Has to be adjacent since we filtered");
@@ -55,12 +61,12 @@ impl Graph {
                     shortest_distance[*adjacent_node] = distance;
                 }
             }
-            if let Some(next_node) = (0..self.number_of_nodes)
-                .filter(|&i| !visited_nodes[i])
-                .sorted_by(|&a, &b| shortest_distance[a].cmp(&shortest_distance[b]))
+            if let Some(next_node) = unvisited_nodes
+                .iter()
+                .sorted_by(|&a, &b| shortest_distance[*a].cmp(&shortest_distance[*b]))
                 .next()
             {
-                current_node = next_node;
+                current_node = *next_node;
             } else {
                 return shortest_distance[node_b];
             }
--- tests/test_dijkstra.rs
+++ tests/test_dijkstra.rs
@@ -18,3 +18,25 @@ fn test_basic_case() {
     assert_eq!(graph.shortest_distance(0, 4), 4);
     assert_eq!(graph.shortest_distance(0, 3), 6);
 }
+
+#[test]
+fn test_other_basic_case() {
+    // Build the graph by making all the points
+    let mut graph = Graph::new(6);
+    graph.zero_out_self_weights();
+    graph.add_weight(0, 1, 9);
+    graph.add_weight(0, 2, 4);
+
+    graph.add_weight(1, 2, 2);
+    graph.add_weight(1, 3, 7);
+    graph.add_weight(1, 4, 3);
+
+    graph.add_weight(2, 3, 1);
+    graph.add_weight(2, 4, 6);
+
+    graph.add_weight(3, 4, 4);
+    graph.add_weight(3, 5, 8);
+
+    graph.add_weight(4, 5, 2);
+    assert_eq!(graph.shortest_distance(0, 5), 11);
+}
